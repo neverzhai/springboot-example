2dd733aba560343907886cbbee52483431eeff34
zhaixiaoshuang <zhaixiaoshuang@jd.com>
2019-12-03 21:44:20 +0800

    [xiaoshuang] temp for integration test

--- src/test/java/com/shuanger/springbootexample/integrationTests/SystemUserControllerIntegrationTest.java
+++ src/test/java/com/shuanger/springbootexample/integrationTests/SystemUserControllerIntegrationTest.java
@@ -3,32 +3,38 @@ package com.shuanger.springbootexample.integrationTests;
 import com.shuanger.springbootexample.VO.SystemUserVO;
 import com.shuanger.springbootexample.params.QueryUserParam;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMybatis
+@ActiveProfiles("ut")
 public class SystemUserControllerIntegrationTest {
 
     @LocalServerPort
     private int port;
+//
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private TestRestTemplate template;
 
     @Test
+    @Sql()
     public void should_query_user_when_given_valid_user_name() {
         QueryUserParam queryUserParam = new QueryUserParam();
         queryUserParam.setUsername("shuanger");
 
-        SystemUserVO systemUserVO = this.testRestTemplate.postForObject("http://localhost:" + port + "/user", queryUserParam, SystemUserVO.class);
+        SystemUserVO systemUserVO = this.template.postForObject("http://localhost:" + port + "/user", queryUserParam, SystemUserVO.class);
 
         assertThat(systemUserVO.getUsername()).isEqualTo("shuanger");
         assertThat(systemUserVO.getUserEmail()).isEqualTo("zhaixiaoshuang@jd.com");
         assertThat(systemUserVO.getUserInfo()).isEqualTo("a programmer");
-
     }
 }
